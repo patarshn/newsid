397f3b39ae0af2149870bb3ba8cff17051156b17
reka amelia <rekaamelia1405@gmail.com>
2020-12-05 19:29:10 +0700

    menu administrasi keuangan

---
+++ application/controllers/admin/Apbd.php
@@ -0,0 +1,125 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Rencana_pembangunan extends Admin_Controller {
+
+    private $_table = 'apbd';
+    private $_folder = 'apbd';
+    private $_mainTitle = 'Anggaran Pendapatan dan Belanja Desa';
+
+    function __construct()
+	{
+        parent::__construct();
+        $this->load->model('Main_m');
+        $this->load->library('breadcrumbcomponent'); 
+    }    
+    
+    function rulesUpdate(){
+        return [
+            ['field' => 'id',
+            'label' => 'id',
+            'rules' => 'required'],
+        ];  
+    }
+
+    function rulesDestroy(){
+        return [
+            ['field' => 'rowdelete[]',
+            'label' => 'rowdelete',
+            'rules' => 'required']
+        ];
+    }
+
+    function index(){
+            
+            $this->breadcrumbcomponent->add('Home', base_url());
+            $this->breadcrumbcomponent->add('Admin', base_url('admin'));  
+            $this->breadcrumbcomponent->add($this->_mainTitle, base_url('admin/'.$this->_folder.'/'));
+
+            $breadcrumb = $this->breadcrumbcomponent->output();
+            $data = array(
+                'breadcrumb' => $breadcrumb,
+                'data' => $this->Main_m->get($this->_table,null)->result(),
+                'title' => $this->_mainTitle,
+                'uri' => $this->uri->segment_array(),
+                'folder' => $this->_folder,
+            );
+
+            $this->load->view('admin/partials/header');
+            $this->load->view('admin/partials/content_sidebar');
+            $this->load->view('admin/partials/content_navbar');
+            $this->load->view('admin/'.$this->_folder.'/index',$data);
+            $this->load->view('admin/partials/content_footer');
+            $this->load->view('admin/partials/footer');
+    }
+
+    function edit($id){
+            
+        $this->breadcrumbcomponent->add('Home', base_url());
+        $this->breadcrumbcomponent->add('Admin', base_url('admin')); 
+        $this->breadcrumbcomponent->add($this->_mainTitle, base_url('admin/'.$this->_folder.'/')); 
+        $this->breadcrumbcomponent->add('Edit', base_url('admin/'.$this->_folder.'/edit/'));
+        $this->breadcrumbcomponent->add($id, base_url('admin/'.$this->_folder.'/edit/'.$id));
+
+        $breadcrumb = $this->breadcrumbcomponent->output();
+        $where = ['id'=>$id];
+        $data = array(
+            'breadcrumb' => $breadcrumb,
+            'data' => $this->Main_m->get($this->_table,$where)->result(),
+            'title' => "Edit ".$this->_mainTitle,
+            'uri' => $this->uri->segment_array(),
+            'folder' => $this->_folder,
+        );
+
+        $this->load->view('admin/partials/header');
+        $this->load->view('admin/partials/content_sidebar');
+        $this->load->view('admin/partials/content_navbar');
+        $this->load->view('admin/'.$this->_folder.'/edit',$data);
+        $this->load->view('admin/partials/content_footer');
+        $this->load->view('admin/partials/footer');
+    }
+
+    public function update(){
+        $validation = $this->form_validation;
+        $validation->set_rules($this->rulesUpdate());
+        if($validation->run()){
+            $_POST = $this->input->post();
+            $id = $_POST['id'];
+            $where = ['id'=>$id];
+            $data = array(
+                'id' => $_POST['id'],
+                'uraian' => $_POST['uraian'],
+                'anggaran' => $_POST['anggaran'],
+                'keterangan' => $_POST['keterangan'],
+                'updated_at' => date('Y-m-d H:i:s'),
+            );
+                       
+            if($this->Main_m->update($data,$this->_table,$where)){
+                $this->session->set_flashdata('success_message', 'Edit form berhasil, terimakasih');
+                $callback = array(
+                    'status' => 'success',
+                    'message' => 'Data berhasil diupdate',
+                    'redirect' => base_url().'admin/'.$this->_folder,
+                );
+            }
+            else{
+                $this->session->set_flashdata('error_message', 'Mohon maaf, pengisian form gagal');
+                $callback = array(
+                    'status' => 'error',
+                    'message' => 'Mohon Maaf, Update data gagal disimpan',
+                );
+            }
+        }
+        else{
+            $this->session->set_flashdata('error_message', validation_errors());
+            $callback = array(
+                'status' => 'error',
+                'message' => validation_errors(),
+            );          
+        }
+        echo json_encode($callback);
+    }
+
+
+
+}
