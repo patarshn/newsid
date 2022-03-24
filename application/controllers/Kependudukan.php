e4f8da89ade605a73be8a4d7b6c4ed2a062f58d6
Patar Martua Doli Siahaan <patarismyname@gmail.com>
2022-03-24 20:32:52 +0700

    Update Kependudukan.php

--- application/controllers/Kependudukan.php
+++ application/controllers/Kependudukan.php
@@ -8,35 +8,56 @@ class Kependudukan extends Frontend_Controller{
         parent::__construct();
         $this->load->helper('url');
         $this->load->model('kependudukan_m');
+        $this->load->library('form_validation');
     }    
+
+    function rulesCheck(){
+        return [
+            ['field' => 'nik', 'label' => 'NIK Pemberi Kuasa', 'rules' => 'required|numeric|min_length[16]|max_length[16]'],
+        ];
+    }
 
     //ACTION
     public function nik($nik){
-        $datapenduduk = $this->kependudukan_m->getSingleNik($nik);
-        if($datapenduduk){
-            if(empty($datapenduduk)){
-                $callback = array(
-                    'status' => 'error',
-                    'message' => 'Data tidak ditemukan',
-                );
+        $validation = $this->form_validation;
+        $data = ['nik' => $nik];
+        $validation->set_data($data);
+        $validation->set_rules($this->rulesCheck());
+        if($validation->run()){
+            $datapenduduk = $this->kependudukan_m->getSingleNik($nik);
+            if($datapenduduk){
+                if(empty($datapenduduk)){
+                    $callback = array(
+                        'status' => 'error',
+                        'message' => 'Data tidak ditemukan',
+                    );
+                }
+                else{
+    
+                    $callback = array(
+                        'status' => 'success',
+                        'data' => $datapenduduk,
+                        'message' => 'Data ditemukan',
+                    );
+                }
             }
             else{
-
+                //$this->session->set_flashdata('error_message', 'Mohon maaf, pengisian form gagal');
                 $callback = array(
-                    'status' => 'success',
-                    'data' => $datapenduduk,
-                    'message' => 'Data ditemukan',
+                    'status' => 'error',
+                    'message' => 'Data gagal dimuat',
                 );
             }
         }
         else{
-            //$this->session->set_flashdata('error_message', 'Mohon maaf, pengisian form gagal');
+            //$this->session->set_flashdata('error_message', validation_errors());
             $callback = array(
                 'status' => 'error',
-                'message' => 'Data gagal dimuat',
+                'message' => validation_errors(),
             );
         }
         echo json_encode($callback);
-    }
+        }
+        
 
 }
